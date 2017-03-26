92522630bdc3580097383113922239c5309c1d40
kinbasco <freepeace13@gmail.com>
2017-03-26 01:00:58 -0400

    Added master layout and load external libraries

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
 var express = require('express');
+var stylus = require('stylus');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 var app = express();
 
+function compile(str, path) {
+  return stylus(str).set('filename', path);
+}
+
 app.set('views', __dirname + '/app/views');
 app.set('view engine', 'jade');
+app.use(logger('dev'));
+app.use(bodyParser());
+app.use(stylus.middleware(
+  {
+    src: __dirname + '/public',
+    compile: compile
+  }
+));
+app.use(express.static(__dirname + '/public'));
 
 app.get('*', function(req, res) {
   res.render('index');
